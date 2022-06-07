1bcfec590eef2ada090e698a5581deea1c4e4aec
nikitha-gudivada <95574915+nikitha-gudivada@users.noreply.github.com>
2022-06-07 22:28:54 +0530

    first commit

---
+++ A2.py
@@ -0,0 +1,14 @@
+student_details={'stud1':{'Name':'Nikitha','Rollno':'19881A0523','ssc':10,'inter':10,'cgpa':9.3,'language':'python'},
+                 'stud2':{'Name':'Ramya','Rollno':'19881A0521','ssc':10,'inter':10,'cgpa':9.3,'language':'python'}}
+for key,value in student_details[input()].items():
+    print(key,":",value)
+'''for key,value in student_details.items():
+    print(key,"Details")
+    for key1,value1 in student_details[key].items():
+        print(key1,":",value1)
+    print("**********")'''
+
+
+
+
+
